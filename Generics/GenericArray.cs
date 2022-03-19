4dd34ab7288cf5e81d991c929f22f34e76304ca6
Cezar Buna <cezar.buna@amdaris.com>
2022-03-19 14:12:09 +0200

    generics assignment(part 1)

---
+++ Generics/GenericArray.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    //Generic array class
+    public class GenericArray<T> : IEnumerable<T>
+    {
+        private int count;
+        public T[] Items;
+
+        public GenericArray(T[] items)
+        {
+            count = items.Length;
+            Items = items;
+        }
+        public T GetByIndex(int index)
+        {
+            return Items[index];
+        }
+        public void SetByIndex(int index,T key)
+        {
+            Items[index] = key;
+        }
+
+        public void SwapItems(int index1, int index2)
+        {
+            (Items[index1], Items[index2]) = (Items[index2], Items[index1]);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (T item in Items)
+                yield return item;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Generics
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var arr = new int[] { 1, 2, 3, 4 };
+
+            GenericArray<int> genericArray = new GenericArray<int>(arr);
+
+            while (true)
+            {
+                Console.WriteLine("Press 1 for getting at index");
+                Console.WriteLine("Press 2 for setting at index");
+                Console.WriteLine("Press 3 for swapping items");
+                Console.WriteLine("Press 4 for displaying array");
+                Console.WriteLine("Press 0 for exiting the program");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 0:
+                        return;
+                    case 1:
+                        {
+                            Console.WriteLine("Type in index");
+                            var index = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine($"The item is: {genericArray.GetByIndex(index)}");
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("Type in index:");
+                            var index = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Type in value(int):");                           
+                            var value = Convert.ToInt32(Console.ReadLine());
+                            genericArray.SetByIndex(index, value);
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("Type in item1(int):");
+                            var item1 = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Type in item2(int):");
+                            var item2 = Convert.ToInt32(Console.ReadLine());
+                            genericArray.SwapItems(item1, item2);
+                            break;
+                        }
+                    case 4:
+                        {
+                            Console.WriteLine("The array is: \n");
+                            foreach (var item in genericArray)
+                                Console.Write($"{item} ");
+                            break;
+                        }
+                   
+                }
+            }
+        }
+    }
+}
